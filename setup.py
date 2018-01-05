71682870b6941d072cadd0068d4c6e5d78b8c53e
Will Rogers <wahrogers@gmail.com>
2018-01-05 13:31:45 +0000

    Correct Apache licence text.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='cssgen',
       version='0.0.1',
       url='https://github.com/dls-controls/cssgen',
-      license='APACHE',
+      license='Apache License 2.0',
       packages=['renderers', 'renderers.css', 'opimodel'],
       install_requires=['lxml'])
